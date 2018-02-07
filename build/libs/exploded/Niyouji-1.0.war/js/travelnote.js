5904ab99f212a3fafe76236c61a7771c49df1f92
JeramTough <1171867004@qq.com>
2018-02-07 22:03:19 +0800

    html成功添加多个travelnotePage元素

--- build/libs/exploded/Niyouji-1.0.war/js/travelnote.js
+++ build/libs/exploded/Niyouji-1.0.war/js/travelnote.js
@@ -14,7 +14,15 @@ const Init = {
         ,
         initTravelnote: function () {
             var travelnoteId = RequestParametersUtil.getParameter("travelnoteId");
-            Model.travelnoteHandler.obtainingTravelnote(travelnoteId);
+            Model.travelnoteHandler.obtainingTravelnote(travelnoteId, function (receivedTravelnote) {
+                View.travelnoteView.setTitle(receivedTravelnote.travelnoteTitle);
+                View.travelnoteView.setDate(receivedTravelnote.createTime);
+
+                receivedTravelnote.travelnotePages.forEach(function (value) {
+                    View.travelnoteView.addPage(value);
+                });
+
+            });
         }
 
     }
@@ -29,18 +37,34 @@ const Controller = {
 const Model = {
     travelnoteHandler: {
         travelnote: null,
-        obtainingTravelnote: function (travelnoteId) {
+        obtainingTravelnote: function (travelnoteId, listener) {
             if (travelnoteId == null) {
                 travelnoteId = 1;
             }
             var url = "getTravelnote.do?travelnoteId=" + travelnoteId;
             $.get(url, function (data, status) {
                 if (status === "success") {
-                    this.travelnote = data;
+                    Model.travelnoteHandler.travelnote = data;
+                    //console.info(Model.travelnoteHandler.travelnote);
+                    listener(Model.travelnoteHandler.travelnote);
                 }
             });
         }
     }
 };
 
-const View = {};
+const View = {
+    travelnoteView: {
+        setTitle: function (title) {
+            $("#travelnote #travelnote_title").html(title);
+        },
+        setDate: function (createTime) {
+            $("#travelnote #travelnote_date").html(createTime.substring(0, 10));
+        },
+        addPage: function (travelnotePage) {
+            var $newTravelnotePage = $("#travelnote_page_template").clone();
+            $("#travelnote .container").append($newTravelnotePage);
+        }
+
+    }
+};
